79f7965e65d2926b557cc9171ed4e859c5c6ae6b
Sebastian Jaschinski <jaschinski@it-freelancer.de>
2022-12-09 11:48:14 +0000

    Added winget install script
    Added winget install script that is supposed to be used for Windows 11 environment setup using ARM template and Custom Script Extension.

---
+++ install.ps1
@@ -0,0 +1,6 @@
+winget install --id Microsoft.PowerShell --silent --accept-package-agreements
+winget install --id Git.Git --silent --accept-package-agreements
+winget install --id Microsoft.VisualStudioCode --silent --accept-package-agreements
+winget install --id Microsoft.VisualStudio.2022.Community --silent --accept-package-agreements --override "--wait --quiet --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.NetWeb"
+winget install --id Docker.DockerDesktop --silent --accept-package-agreements
+wsl --update
